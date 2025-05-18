ea4265dbe693323ba66a546c61000e80f1242641
vmascotte <victor@mascotteplus.fr>
2025-05-19 00:12:05 +0200

    ajusta numero maximo de cores

--- palette.py
+++ palette.py
@@ -7,6 +7,13 @@ def extrair_cores(imagem, n_cores=5):
     img = Image.open(imagem).convert('RGB')
     img = img.resize((200, 200))
     pixels = np.array(img).reshape(-1, 3)
+    n_cores_max = len(np.unique(pixels, axis=0))
+    if n_cores > n_cores_max:
+        print(
+            f"Número de cores solicitado ({n_cores}) excede o máximo possível ({n_cores_max}). "
+            f"Ajustando para {n_cores_max}."
+        )
+        n_cores = n_cores_max
     kmeans = KMeans(n_clusters=n_cores, n_init="auto")
     kmeans.fit(pixels)
     cores = kmeans.cluster_centers_.astype(int)
