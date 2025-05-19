2c50b6cb55fbc18154cfafe1d939ef1a95d4d77e
vmascotte <victor@mascotteplus.fr>
2025-05-19 20:44:35 +0200

    Add pie chart feature with percentages

--- web_ui.py
+++ web_ui.py
@@ -1,11 +1,14 @@
 import gradio as gr
-from palette import extrair_cores, save_palette
+from palette import extrair_cores_percentual, save_palette
+import matplotlib.pyplot as plt
+from io import BytesIO
+from PIL import Image
 import subprocess
 import os
 
 
 def gerar_paleta(imagem, num_cores):
-    cores = extrair_cores(imagem, n_cores=num_cores)
+    cores, porcentagens = extrair_cores_percentual(imagem, n_cores=num_cores)
     blocos = "".join(
         f"<div style='width:40px;height:40px;background:{c};'></div>" for c in cores
     )
@@ -14,7 +17,18 @@ def gerar_paleta(imagem, num_cores):
         + "\n".join(cores)
         + "</pre>"
     )
-    return html, cores
+
+    fig, ax = plt.subplots()
+    labels = [f"{p*100:.1f}%" for p in porcentagens]
+    ax.pie(porcentagens, colors=cores, labels=labels, startangle=90)
+    ax.axis("equal")
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    plt.close(fig)
+    buf.seek(0)
+    grafico = Image.open(buf)
+
+    return html, grafico, cores
 
 
 def salvar_paleta(caminho, cores):
@@ -47,6 +61,7 @@ def main():
             entrada_imagem = gr.Image(type="filepath", label="Imagem")
             entrada_num_cores = gr.Slider(1, 10, value=5, step=1, label="N\u00famero de cores")
         saida = gr.HTML()
+        grafico = gr.Image(label="Distribuição")
         cores_state = gr.State([])
         caminho_paleta = gr.Textbox("paleta.png", label="Salvar como")
         status = gr.Textbox(label="Status", interactive=False)
@@ -56,7 +71,7 @@ def main():
         atualizar = gr.Button("Atualizar Programa")
 
         executar.click(
-            gerar_paleta, [entrada_imagem, entrada_num_cores], [saida, cores_state]
+            gerar_paleta, [entrada_imagem, entrada_num_cores], [saida, grafico, cores_state]
         )
         salvar.click(salvar_paleta, [caminho_paleta, cores_state], status)
         atualizar.click(atualizar_programa, outputs=status)
